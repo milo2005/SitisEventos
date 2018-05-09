3971f892970edac2f2c9aa3f112629485a7fde4f
Dario Chamorro <dario.chamorro.v@gmail.com>
2018-05-09 16:06:26 -0500

    detail event implemented

--- app/src/main/java/org/test/eventos/DetailEventActivity.kt
+++ app/src/main/java/org/test/eventos/DetailEventActivity.kt
@@ -2,11 +2,27 @@ package org.test.eventos
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_detail_event.*
+import org.test.eventos.models.Evento
+import org.test.eventos.provider.EventData
 
 class DetailEventActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_event)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        collapsingToolbar.title = "Nombre Evento"
+
+        val data = EventData.listEvents()[0] as Evento
+        img.setImageURI(data.imagen)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        finish()
+        return super.onOptionsItemSelected(item)
     }
 }
